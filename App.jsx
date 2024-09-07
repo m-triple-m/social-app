df86461c660172a2397cec64598ddff78e071b9d
m-triple-m <triplem656@gmail.com>
2024-09-07 12:53:17 +0530

    authentication

--- App.jsx
+++ App.jsx
@@ -4,14 +4,57 @@ import { StyleSheet, Text, View } from 'react-native';
 import AddPost from './screens/AddPost';
 import Feed from './screens/Feed';
 import { NavigationContainer } from '@react-navigation/native';
+import { IconButton } from 'react-native-paper';
+import Signup from './screens/Signup';
+import { useEffect, useState } from 'react';
+import Login from './screens/Login';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import app from './firebaseConfig';
 
+const auth = getAuth(app);
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  const [signupOpen, setSignupOpen] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUserLoggedIn(true);
+      } else {
+        setUserLoggedIn(false);
+      }
+    });
+  }, [])
+
+
+  const openLogin = () => {
+    setLoginOpen(true);
+    setSignupOpen(false);
+  }
+
+  const openSignup = () => {
+    setSignupOpen(true);
+    setLoginOpen(false);
+  }
+
   return (
     <NavigationContainer>
+      <Signup visible={signupOpen} setVisible={setSignupOpen} openLogin={openLogin} />
+      <Login visible={loginOpen} setVisible={setLoginOpen} openSignup={openSignup} />
       <Tab.Navigator>
-        <Tab.Screen name="Feed" component={Feed} />
+        <Tab.Screen name="Feed" component={Feed} options={{
+          headerRight: () => !userLoggedIn ? <IconButton
+            icon={'account-circle'}
+            onPress={() => { setLoginOpen(true); }} />
+            :
+            <IconButton
+              icon={'logout'}
+              onPress={() => { setUserLoggedIn(false); }} />
+        }} />
         <Tab.Screen name="Addpost" component={AddPost} />
       </Tab.Navigator>
     </NavigationContainer>
---
+++ screens/Login.jsx
@@ -0,0 +1,91 @@
+import { View, Text, Modal } from 'react-native'
+import React from 'react'
+import { Controller, useForm } from 'react-hook-form';
+import { Button, TextInput } from 'react-native-paper';
+import app from '../firebaseConfig';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+const auth = getAuth(app);
+
+const Login = ({ visible, setVisible, openSignup }) => {
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+    });
+
+    const onSubmit = (data) => {
+        // alert(JSON.stringify(data)); 
+
+        signInWithEmailAndPassword(auth, data.email, data.password)
+            .then((result) => {
+                const { user } = result;
+                console.log(user);
+                alert('Logged in successfully');
+            }).catch((err) => {
+                console.log(err);
+                alert(err.message);
+            });
+    };
+
+    return (
+        <Modal visible={visible} onRequestClose={() => { setVisible(false) }}>
+            <View>
+                <Text>Login</Text>
+
+                <Controller
+                    control={control}
+                    rules={{
+                        required: { message: 'Email is required', value: true }
+                    }}
+
+                    render={({ field }) => {
+                        return <TextInput
+                            onBlur={field.onBlur}
+                            onChangeText={field.onChange}
+                            value={field.value}
+                            label={"Email Address"}
+                            error={errors.email}
+                        />
+                    }}
+                    name='email'
+                />
+                <Text>{errors.email?.message}</Text>
+
+
+                <Controller
+                    control={control}
+                    rules={{
+                        required: { message: 'Password is required', value: true },
+                    }}
+
+                    render={({ field }) => {
+                        return <TextInput
+                            secureTextEntry
+                            onBlur={field.onBlur}
+                            onChangeText={field.onChange}
+                            value={field.value}
+                            label={"Password"}
+                            error={errors.password}
+                        />
+                    }}
+                    name='password'
+                />
+                <Text>{errors.password?.message}</Text>
+
+                <Button onPress={handleSubmit(onSubmit)} mode='contained'>Login</Button>
+
+                <Button onPress={openSignup} style={{ marginTop: 20 }} mode='outlined'> Not Registered Yet? </Button>
+
+            </View>
+        </Modal>
+    )
+}
+
+export default Login;
